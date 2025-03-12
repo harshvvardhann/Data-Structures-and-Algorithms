eeac783625d2e7b553d28395e49b393f28ae0a03
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-03-13 00:14:02 +0530

    .

---
+++ Arrays/Easy/Sum3Closest.java
@@ -0,0 +1,29 @@
+package Arrays.Easy;
+
+import java.util.Arrays;
+
+class Sum3Closest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int maxClosestSum = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == target){
+                    return sum;
+                }
+                if(Math.abs(target-sum) < Math.abs(target-maxClosestSum)){
+                    maxClosestSum = sum;
+                }
+                if(sum<target){
+                    left++;
+                }else if(sum>target){
+                    right--;
+                }
+            }
+        }
+        return maxClosestSum;
+    }
+}
