93bf5d0acfc7ce0b870ce4ec583ec4e5307d9057
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-10-01 00:55:18 +0530

    Graphs: Number of provinces OR Count Different components

---
+++ Graphs/NumberOfProvinces.java
@@ -0,0 +1,25 @@
+package Graphs;
+class NumberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean visited[] = new boolean[n];
+        int count = 0;
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                count++;
+                dfs(isConnected,visited,i);
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(int isConnected[][],boolean visited[],int i){
+        visited[i] = true;
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j] == 1 && !visited[j]){
+                dfs(isConnected,visited,j);
+            }
+        }
+    }
+}
